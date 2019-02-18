23fee1e87be85ef575cb3f71c45ec29aefe01c2a
viniciusmsantos <viniciusmarsan@gmail.com>
2019-02-18 13:32:09 -0300

    Corrige imagens

--- src/app/shared/coracao.model.ts
+++ src/app/shared/coracao.model.ts
@@ -1,8 +1,8 @@
 export class Coracao {
     constructor(
         public cheio: boolean,
-        public urlCoracaoCheio: string = "/assets/coracao_cheio.png",
-        public urlCoracaoVazio: string = "/assets/coracao_vazio.png"
+        public urlCoracaoCheio: string = "./assets/coracao_cheio.png",
+        public urlCoracaoVazio: string = "./assets/coracao_vazio.png"
     ) {}
 
     public exibeCoracao(): string {
